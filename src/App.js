5d1cd230a7a7462a2f7d76e858341bcc955b51f1
THEjoezack <me@joezack.com>
2017-04-08 21:59:06 -0400

    Plumbing inputs to html

--- src/App.js
+++ src/App.js
@@ -5,9 +5,40 @@ import player from './ToneGenerator';
 import scribble from 'scribbletune';
 
 class App extends Component {
+
   constructor(props) {
     super(props);
-    this.state = { isPlaying: false };
+    this.state = {
+      isPlaying: props.isPlaying,
+      bpm: props.bpm,
+      noteLength: props.noteLength
+    };
+  }
+  
+  updateBpm(event) {
+    var value = this.restrict(event.target.value, this.props.bpmMin, this.props.bpmMax);
+    this.recalibrate({ bpm: value });
+  }
+
+  updateNoteLength(event) {
+    this.recalibrate({ noteLength: event.target.value });
+  }
+
+  recalibrate(options) {
+    this.setState(options);
+    if(this.state.isPlaying) {
+      this.playOrPause();
+    }
+  }
+
+  restrict(value, min, max) {
+    return Math.max(
+      min,
+      Math.min(
+        max,
+        value
+      )
+    );
   }
 
   playOrPause() {
@@ -19,12 +50,12 @@ class App extends Component {
         .map(x => { return scribble.chord(x) })
         .reduce((chords, c) => { return chords.concat(c); }, []);
       //var scale = scribble.scale('c', 'major', 3);
+
       player.start(
         {
           notes: chords,
-          bpm: 120,
-          noteLength: '8n',
-          measures: 16
+          bpm: this.state.bpm,
+          noteLength: this.state.noteLength
         }
       );
     }
@@ -37,14 +68,37 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Music Practice App</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button onClick={this.playOrPause.bind(this)}>{this.state.isPlaying ? 'Stop' : 'Play'}</button>
+        <div>
+          <label htmlFor="bpm">BPM:</label>
+          <input type="number" id="bpm" value={this.state.bpm} onChange={this.updateBpm.bind(this)} min="0" max="300" />
+        </div>
+        <div>
+          <label htmlFor="noteLength">Note Length:</label>
+          <select value={this.state.noteLength} onChange={this.updateNoteLength.bind(this)}>
+            <option value="1n">Whole Note</option>
+            <option value="2n">Half Note</option>
+            <option value="4n">Quarter Note</option>
+            <option value="8n">Eighth Note</option>
+            <option value="16n">Sixteenth Note</option>
+          </select>
+        </div>
+
+        <div>
+          <button onClick={this.playOrPause.bind(this)}>{this.state.isPlaying ? 'Stop' : 'Play'}</button>
+        </div>
       </div>
     );
   }
 }
+
+App.defaultProps = {
+  bpm: 120,
+  bpmMin: 1,
+  bpmMax: 300,
+  isPlaying: false,
+  noteLength: '8n'
+};
+
 export default App;
--- src/ToneGenerator.js
+++ src/ToneGenerator.js
@@ -1,20 +1,20 @@
 import Tone from 'tone';
 
+const synth = new Tone.Synth({
+    "oscillator" : {
+        "type" : "pwm",
+        "modulationFrequency" : 0.9
+    },
+    "envelope" : {
+        "attack" : 0.00,
+        "decay" : 0.5,
+        "sustain" : 0.2,
+        "release" : 0.5,
+    }
+}).toMaster();
+
 export default {
     start: function(options) {
-        var synth = new Tone.Synth({
-            "oscillator" : {
-                "type" : "pwm",
-                "modulationFrequency" : 0.9
-            },
-            "envelope" : {
-                "attack" : 0.00,
-                "decay" : 0.5,
-                "sustain" : 0.2,
-                "release" : 0.5,
-            }
-        }).toMaster();
-
         var notes = [];
         for(var i = 0; i < options.notes.length; i++) {
             var note = options.notes[i].toUpperCase();
@@ -26,23 +26,7 @@ export default {
                     dur: options.noteLength
                 });
         }
-
-        // var part = new Tone.Part(
-        //     function(time, event){
-        //         //the events will be given to the callback with the time they occur
-        //         synth.triggerAttackRelease(event.note, event.dur, time)
-        //     }, 
-        //     notes
-        // );
-
-        // //start the part at the beginning of the Transport's timeline
-        // part.start(0)
-
-        // //loop the part 3 times
-        // part.loop = 4
-        var measures = Math.ceil(notes.length / parseInt(options.noteLength)); // would rather set the signature!
-        // part.loopEnd = measures.toString() + 'm'
-
+        
         var i = 0;
         var loop = new Tone.Loop(function(time){
             var note = notes[i % notes.length].note;
@@ -50,7 +34,7 @@ export default {
             i++;
         }, options.noteLength);
 
-        loop.start("1m").stop("100m");
+        loop.start("1m"); // basically just play forever
 
         Tone.Transport.bpm.value = options.bpm;
         Tone.Transport.start();
